8331c6deaa34b30dadda23174f23dececcf34afd
Уматай Нургиса <nurgissa_umatay@epam.com>
2020-11-10 02:32:58 +0600

    feat: integrate FE with changed API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com',
-  order: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com',
-  import: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com',
-  bff: 'https://p6cq8jgiid.execute-api.eu-west-1.amazonaws.com'
+  product: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
