8e779dc377f8ff974d1e53d2efcf4cd275cd1a27
Уматай Нургиса <nurgissa_umatay@epam.com>
2020-11-17 17:32:33 +0600

    feat: Add endpoint for import service and sync with main repo

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://hdhgxbo5gi.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://pnes1azyil.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
